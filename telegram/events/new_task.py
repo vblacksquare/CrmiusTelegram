4d4897e69b709f158fe1263df0deeb1fb9ff99c2
vblacksquare <>
2025-07-05 01:16:03 +0300

    init

--- telegram/events/new_task.py
+++ telegram/events/new_task.py
@@ -1,5 +1,6 @@
 
 from loguru import logger
+import bs4
 
 from emitter import emitter, EventType
 
@@ -29,7 +30,7 @@ async def new_task(notification: Notification):
         "task_id": task.id,
         "sender": sender,
         "reciever": reciever,
-        "text": message.text if message else None,
+        "text": prepare_text(message.text) if message else None,
         "type": notification.type,
         "title": task.title
     }
@@ -40,3 +41,53 @@ async def new_task(notification: Notification):
         EventType.send_task,
         **kwargs
     )
+
+
+def prepare_text(message: str) -> str:
+    soup = bs4.BeautifulSoup(message if isinstance(message, str) else message.text, "html.parser")
+
+    for comment in soup.find_all(string=lambda text: isinstance(text, bs4.element.Comment)):
+        comment.extract()
+
+    is_p_like = False
+
+    p_tag = soup.find("p")
+    if p_tag:
+        is_p_like = True
+
+    for tag in soup.find_all():
+        if isinstance(tag, bs4.element.Tag):
+            if tag.name == "p":
+                tag.insert_before("\n")
+                tag.unwrap()
+
+            elif tag.name == "br":
+                if not is_p_like:
+                    tag.insert_before("\n")
+                tag.unwrap()
+
+            elif tag.name not in ["b", "strong", "i", "em", "u", "s", "strike", "del", "tg-spoiler", "a", "code", "pre"]:
+                tag.unwrap()
+
+            else:
+                tag.attrs = {key: value for key, value in tag.attrs.items() if key == "href"}
+
+    decoded_text = soup.decode()
+
+    cleared_text = ""
+    for i, char in enumerate(decoded_text):
+        if char != "\n":
+            cleared_text = decoded_text[i:]
+            break
+
+    text = cleared_text
+
+    if len(text) > 1000:
+        text = text[:1000] + f"... (+{len(text) - 1000})"
+
+    if isinstance(message, str):
+        return text
+
+    else:
+        message.text = text
+        return message
